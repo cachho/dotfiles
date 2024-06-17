16b71097de20fb50f4c489167fabfb99bc8a4cf4
cachho <admin@ch-webdev.com>
2024-06-17 16:51:09 +0200

    fix: verseion

--- run_3_fish_plugins.fish
+++ run_3_fish_plugins.fish
@@ -13,7 +13,7 @@ fisher install jorgebucaran/nvm.fish
 
 ## Install specific target version
 # Set target version variable
-NODE_TARGET_VERSION = "v18.17"
+NODE_TARGET_VERSION = "18.17"
 
 set --universal nvm_default_version $NODE_TARGET_VERSION
 nvm install $NODE_TARGET_VERSION
