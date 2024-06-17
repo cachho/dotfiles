e01b353dc608b16c2b071ad1e5436a588881074b
cachho <admin@ch-webdev.com>
2024-06-17 16:12:53 +0200

    fix: pyenv

--- run_1_pyenv.sh
+++ run_1_pyenv.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-# Install pyenv
-echo "Installing pyenv..."
-curl https://pyenv.run | bash
+
+
+####### pyenv ########
+
+# Check if ~/.pyenv exists
+if [ -d "$HOME/.pyenv" ]; then
+    echo "~/.pyenv already exists. Skipping installation."
+else
+    # Install pyenv
+    echo "Installing pyenv..."
+    curl https://pyenv.run | bash
+fi
