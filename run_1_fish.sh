745d93257fc06da512bb5b2de3edadc3551ece2a
cachho <admin@ch-webdev.com>
2024-06-17 15:10:19 +0200

    chore: cleanup

--- run_1_fish.sh
+++ run_1_fish.sh
@@ -3,7 +3,8 @@
 # Install Fish Shell
 sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt update
-sudo apt install fish
+sudo apt install -y fish
 
 # Set fish as the default shell
+echo "Setting fish as the default shell..."
 chsh -s $(which fish)
