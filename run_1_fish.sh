ee821069eab93163296dfed31963cca9a4b19c2a
cachho <admin@ch-webdev.com>
2024-06-17 15:06:02 +0200

    chore: organize scripts

--- run_1_fish.sh
+++ run_1_fish.sh
@@ -7,20 +7,3 @@ sudo apt install fish
 
 # Set fish as the default shell
 chsh -s $(which fish)
-
-# Install ssh agent
-wget https://gitlab.com/kyb/fish_ssh_agent/raw/master/functions/fish_ssh_agent.fish -P ~/.config/fish/functions/
-echo "AddKeysToAgent yes" >> ~/.ssh/config
-
-# Switch to fish shell
-fish
-
-# Install Fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-fisher -v
-
-# Install Plugins
-
-## Z Script
-fisher install jethrokuan/z
-
