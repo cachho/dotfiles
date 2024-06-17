1a083e2a0680db859ab238698f2b60264453ab94
cachho <admin@ch-webdev.com>
2024-06-17 15:58:41 +0200

    chore: add echos

--- run_0_dependencies.sh
+++ run_0_dependencies.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+echo "Updating and upgrading packages..."
 sudo apt update
+sudo apt upgrade -y
 
 # Keychain as a ssh-agent alternative
+echo "Installing keychain..."
 sudo apt install -y keychain
-
-# Upgrade all
-sudo apt upgrade -y
--- run_1_fish.sh
+++ run_1_fish.sh
@@ -2,7 +2,7 @@
 
 # Install Fish Shell
 echo "Installing Fish Shell..."
-sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt-add-repository -y ppa:fish-shell/release-3
 sudo apt update
 sudo apt install -y fish
 echo "Fish Shell installation complete!"
